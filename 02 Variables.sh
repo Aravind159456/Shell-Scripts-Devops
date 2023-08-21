2956dc3939429bc112105fe11fd33270d2f10933
Aravind159456 <krisna.aravind@gmail.com>
2023-08-21 22:38:24 +0530

    Variable shell scipting

---
+++ 02 Variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Hello World, I am learning Devops and AWS"
+
+PERSON1=Sachin
+PERSON2=Aravind
+
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am doing fine $PERSON1"
